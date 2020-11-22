62d5c76cb25463cf289062f550f5f48a29245b7d
jacovb <vbiljon@gmail.com>
2020-11-22 19:34:42 +0000

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import { listSongs } from './graphql/queries';
+import { updateSong } from './graphql/mutations';
 import { useEffect, useState } from "react";
+import { Paper, IconButton } from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 Amplify.configure(awsconfig);
 
@@ -26,6 +29,22 @@ function App() {
     }
   }
 
+  const addLike = async(idx) => {
+    try {
+      const song = songs[idx];
+      song.like = song.like + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+      
+      const songData = await API.graphql(graphqlOperation(updateSong, {input: song}))
+      const songList = [...songs];
+      songList[idx] = songData.data.updateSong;
+      setSongs(songList);
+
+    } catch (error) {
+      console.log('error on adding Like to song', error)
+    }
+  }
 
   return (
     <div className="App">
@@ -34,6 +53,30 @@ function App() {
         <h2>My App Content</h2>
         {/* this is where react router or other single page app content could go */}
       </header>
+      <div className="songlist">
+        {songs.map((song, idx) => {
+          return (
+            <Paper variant="outlined" elevation={2} key={`song.${idx}`}>
+              <div className="songCard">
+                  <IconButton aria-label="play">
+                    <PlayArrowIcon />
+                  </IconButton>
+                <div>
+                  <div className="songTitle">{song.name}</div>
+                  <div className="songOwner">{song.owner}</div>
+                </div>
+                <div>
+                  <IconButton aria-label="like" onClick={() => addLike(idx)}>
+                    <FavoriteIcon />
+                  </IconButton>
+                  {song.like}
+                </div>
+                  <div className="songDescription">{song.description}</div>
+              </div>
+            </Paper>
+          )
+        })}
+      </div>
     </div>
   );
 }
